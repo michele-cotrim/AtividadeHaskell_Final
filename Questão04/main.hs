1b56bda765b5668936d5d1a1296a74ac0c77f3b6
Matheus Cardoso <dacachmatheus@gmail.com>
2021-10-02 17:02:57 -0300

    Add printing to terminal

--- Questão04/main.hs
+++ Questão04/main.hs
@@ -5,8 +5,12 @@ main = do
     let splitWords = words contents
     let count = countStrings splitWords
     let dict = addToDict splitWords
-    let result = ordsim dict
-    return result
+    let keyValuePairs = ordsim dict
+    let result = printToTerminal keyValuePairs
+    putStrLn "### word - count ###\n"
+    putStrLn result
+    return ()
+
 
 countStrings :: [String] -> Int
 countStrings [] = 0
@@ -18,3 +22,14 @@ addToDict x = addToDict' x novo
 addToDict' :: [String] -> Dicionario -> Dicionario
 addToDict' [] curDict = curDict
 addToDict' (x : xs) curDict = addToDict' xs (colocar x curDict)
+
+printToTerminal :: [(String, Int)] -> String
+printToTerminal x = printToTerminal' x ""
+
+printToTerminal' :: [(String, Int)] -> String -> String
+printToTerminal' [] curString = curString
+printToTerminal' (pair : rest) curString = printToTerminal' rest (curString ++ (pairToString pair) ++ "\n")
+
+pairToString :: (String, Int) -> String
+pairToString (a, b) = a ++ " - " ++ (show b)
+-- pairToString pair = (fst pair) ++ " - " ++ (show (snd pair)) 
