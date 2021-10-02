c54227dfd601c497ae500ef4d79d5e667c9d2148
Matheus Cardoso <dacachmatheus@gmail.com>
2021-10-02 19:06:04 -0300

    Add function to generate all possible mappings

--- Questão05/main.hs
+++ Questão05/main.hs
@@ -9,14 +9,16 @@ main = do
     let formula = (read contents :: Formula)
     let uniqueVars = vars formula
     let map = createMapping uniqueVars
-    print (getValue "A" map)
+    let allMappings = generateAll uniqueVars []
+    print (allMappings)
+    print (evaluate formula (allMappings !! 2))
     return ()
-
 
 toBinary :: Int -> String
 toBinary 0 = "0"
 toBinary 1 = "1"
 toBinary x = toBinary (div x 2) ++ show (mod x 2)
+
 
 type Name = String
 vars :: Formula -> [Name] 
@@ -25,15 +27,24 @@ vars (Not x) = vars x
 vars (And x y) = vars x `union` vars y
 vars (Or x y) = vars x `union` vars y
 
+evaluate :: Formula -> Mapping -> Bool
+evaluate (Var x) m = getValue x m
+evaluate (Not x) m = not (evaluate x m)
+evaluate (And x y) m = and [(evaluate x m), (evaluate y m)]
+evaluate (Or x y) m = or [(evaluate x m), (evaluate y m)]
+
 createMapping :: [Name] -> [(Name, Bool)]
 createMapping x = mapping' x []
 
 type Mapping = [(Name, Bool)]
 mapping' :: [Name] -> Mapping -> Mapping
 mapping' [] curList = curList
-mapping' (x : xs) curList = mapping' xs (curList ++ [(x, False)])
+mapping' (x : xs) curList = mapping' xs (curList ++ [(x, True)])
 
 getValue :: Name -> Mapping -> Bool
 getValue name ((n, v) : m) = if n == name then v   
                             else getValue name m 
 
+generateAll :: [Name] -> Mapping -> [Mapping]
+generateAll [] m = [m]
+generateAll (x : xs) m = (generateAll xs ([(x, True)] ++ m)) `union` (generateAll xs ([(x, False)] ++ m))
