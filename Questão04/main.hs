4c83f0208377846a7e8574965ad8a53466c5e05a
Matheus Cardoso <dacachmatheus@gmail.com>
2021-10-02 16:45:27 -0300

    Add function to add words to dictionary

--- Questão04/main.hs
+++ Questão04/main.hs
@@ -1,9 +1,20 @@
+import Dicionario
+
 main = do
     contents <- readFile "input.txt"
     let splitWords = words contents
     let count = countStrings splitWords
-    return count
+    let dict = addToDict splitWords
+    let result = ordsim dict
+    return result
 
 countStrings :: [String] -> Int
 countStrings [] = 0
 countStrings (x : xs) = 1 + countStrings xs
+
+addToDict :: [String] -> Dicionario
+addToDict x = addToDict' x novo
+
+addToDict' :: [String] -> Dicionario -> Dicionario
+addToDict' [] curDict = curDict
+addToDict' (x : xs) curDict = addToDict' xs (colocar x curDict)
