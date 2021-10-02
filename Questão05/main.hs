affc98cfa7adf985f88d608636df9a4f06519379
Matheus Cardoso <dacachmatheus@gmail.com>
2021-10-02 17:51:48 -0300

    Add diversao, obrigado Stenio

--- Questão05/main.hs
+++ Questão05/main.hs
@@ -1,7 +1,18 @@
-data Formula = Var String | Not Formula | And Formula Formula | Or Formula Formula
+data Formula = Var String | Not Formula | And Formula Formula | Or Formula Formula deriving (Read, Show)
 
 main = do
     -- fileName <- getLine
     -- contents <- readFile fileName
     contents <- readFile "input.txt"
-    return contents
+    let formula :: Formula
+        formula = read contents
+    return (diversao formula)
+
+
+toBinary :: Int -> String
+toBinary 0 = "0"
+toBinary 1 = "1"
+toBinary x = toBinary (div x 2) ++ show (mod x 2)
+
+diversao :: Formula -> Int
+diversao x = 666
