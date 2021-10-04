9a5990831fe39fbb4984a011db242f6955925593
Matheus Cardoso <dacachmatheus@gmail.com>
2021-10-03 22:37:05 -0300

    Clean up code and add documentation

--- Questão04/main.hs
+++ Questão04/main.hs
@@ -3,34 +3,35 @@ import Dicionario
 main = do
     fileName <- getLine
     contents <- readFile fileName
+    -- split by empty space
     let splitWords = words contents
-    let count = countStrings splitWords
+    -- binary tree dictionary to count occurrences
     let dict = addToDict splitWords
+    -- list all pairs in dictionary
     let keyValuePairs = ordsim dict
+    let header = "### word - count ###\n"
     let result = printToTerminal keyValuePairs
-    putStrLn "### word - count ###\n"
-    putStrLn result
+    -- save to file
+    writeFile "word_count.txt" (header ++ result)
     return ()
 
-
-countStrings :: [String] -> Int
-countStrings [] = 0
-countStrings (x : xs) = 1 + countStrings xs
-
+-- Add all words to the binary tree dictionary
 addToDict :: [String] -> Dicionario
 addToDict x = addToDict' x novo
 
+-- Internal implementation of addToDict
 addToDict' :: [String] -> Dicionario -> Dicionario
 addToDict' [] curDict = curDict
 addToDict' (x : xs) curDict = addToDict' xs (colocar x curDict)
 
+-- Converts pair of values to printable string
+pairToString :: (String, Int) -> String
+pairToString (a, b) = a ++ " - " ++ (show b)
+
+-- Printing functions
 printToTerminal :: [(String, Int)] -> String
 printToTerminal x = printToTerminal' x ""
 
 printToTerminal' :: [(String, Int)] -> String -> String
 printToTerminal' [] curString = curString
 printToTerminal' (pair : rest) curString = printToTerminal' rest (curString ++ (pairToString pair) ++ "\n")
-
-pairToString :: (String, Int) -> String
-pairToString (a, b) = a ++ " - " ++ (show b)
--- pairToString pair = (fst pair) ++ " - " ++ (show (snd pair)) 
